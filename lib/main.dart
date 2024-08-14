716033872af2734cf62916d9e80599545c54f179
birdfromhell <ababilmustaqim@proton.me>
2024-08-14 14:14:17 +0700

    chore: Refactor main.dart and add assets folder for app images

--- lib/main.dart
+++ lib/main.dart
@@ -8,50 +8,36 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  void _showAlertDialog(BuildContext context) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('Alert'),
-          content: Text('This is an alert dialog.'),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: Text('Cancel'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: Text('OK'),
-            ),
-          ],
-        );
-      },
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Alert Dialog'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            _showAlertDialog(context);
-          },
-          child: Text('Show Alert'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Web Shop'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Welcome to our Web Shop!',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 20),
+              Image.asset(
+                'assets/appimages/tshirt-01.jpg',
+                width: 200,
+                height: 200,
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  // Add your button functionality here
+                },
+                child: Text('Buy Now'),
+              ),
+            ],
+          ),
         ),
       ),
     );
